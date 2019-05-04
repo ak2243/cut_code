3073725b7672db31ab5b7f33b0353d74c1430f43
Arjun Khanna <arjunk2022@heeadroyce.org>
2019-05-04 14:49:29 -0700

    Worked on Variable Block Class a bit on Friday the 3rd

---
+++ cutCode/src/application/VariableBlock.java
@@ -0,0 +1,27 @@
+package application;
+
+public class VariableBlock<T> extends Block<T> {
+	T value;
+	String varName;
+	
+	public void setName(String s)
+	{
+		varName = s;
+	}
+	
+	public String getName() {return varName;}
+	
+	public void setValue(T val)
+	{
+		value = val;
+	}
+	
+	@Override
+	public T execute() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+	
+	
+	
+}
