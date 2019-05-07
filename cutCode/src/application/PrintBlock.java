b15d7a154ac162f9dee603840b5650c5c0ed197b
Arjun Khanna <arjunk2022@heeadroyce.org>
2019-05-06 18:44:52 -0700

    Changed Block, PrintBlock, and VariableBlock classes. Added Builder.java class

--- cutCode/src/application/PrintBlock.java
+++ cutCode/src/application/PrintBlock.java
@@ -2,12 +2,13 @@ package application;
 
 public class PrintBlock extends Block<String> {
 	private String print;
+
 	@Override
 	public String execute() {
 		// TODO Auto-generated method stub
 		return print;
 	}
-	
+
 	/**
 	 * @param print the print to set
 	 */
--- cutCode/src/application/VariableBlock.java
+++ cutCode/src/application/VariableBlock.java
@@ -3,25 +3,23 @@ package application;
 public class VariableBlock<T> extends Block<T> {
 	T value;
 	String varName;
-	
-	public void setName(String s)
-	{
+
+	public void setName(String s) {
 		varName = s;
 	}
-	
-	public String getName() {return varName;}
-	
-	public void setValue(T val)
-	{
+
+	public String getName() {
+		return varName;
+	}
+
+	public void setValue(T val) {
 		value = val;
 	}
-	
+
 	@Override
 	public T execute() {
 		// TODO Auto-generated method stub
 		return value;
 	}
-	
-	
-	
+
 }
