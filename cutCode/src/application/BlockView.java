4db316b0c4ef07c17e71c8da22edcb4d8fa1ddbf
Peter Timpane <petert2022@headroyce.org>
2019-05-17 12:46:46 -0700

    Made BlockView extend H-Box

--- cutCode/src/application/BlockView.java
+++ cutCode/src/application/BlockView.java
@@ -1,18 +1,33 @@
 package application;
 
 import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.*;
 import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+
 import javafx.geometry.*;
 
-public class BlockView extends Button {
+public class BlockView extends HBox {
+	private BlockView nextBlock;
+	private BlockView nestedIn;
+	
+	
 	public BlockView(int width, int height, String ID) {
 		// Paint p = new Paint();
 		// this.setFill(p);
 		this.setId(ID);
 		this.setMinHeight(height);
 		this.setMinWidth(width);
-		this.setText(ID);
+		
+	}
+
+	public BlockView getNextBlock() {
+		return nextBlock;
+	}
+
+	public void setNextBlock(BlockView nextBlock) {
+		this.nextBlock = nextBlock;
 	}
 }
--- cutCode/src/application/LayoutView.java
+++ cutCode/src/application/LayoutView.java
@@ -1,111 +1,145 @@
 package application;
 
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.beans.binding.Bindings;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+
 import java.util.*;
 
-public class LayoutView extends Pane{
+public class LayoutView extends Pane {
 
 	private AnchorPane playArea;
 	private VBox blockStorage;
 	private ArrayList<BlockView> blocks;
 	private BorderPane layout;
-	
-	
-	public LayoutView() {
+	private BlockView firstBlock;
+
+	public LayoutView(double width, double height) {
+
 		layout = new BorderPane();
 		layout.setPadding(new Insets(10));
 		this.getChildren().add(layout);
-		
-		
+
 		playArea = new AnchorPane();
+		playArea.setMinSize(width - 300, height);
+		System.err.println(width);
 		layout.setCenter(playArea);
-		
-		blockStorage = new VBox();
+
+		blockStorage = makePalette();
+
 		layout.setLeft(blockStorage);
-		
+
 		blocks = new ArrayList<BlockView>();
+
+		blockStorage.setBorder(new Border(
+				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		
-		BlockView addMe;
-		
-		Button ifBlock = new Button();
-		ifBlock.setPrefWidth(100);
-		ifBlock.setPrefHeight(50);
-		
-		Mouser mouser = new Mouser(this);
-		ifBlock.setOnMousePressed(mouser);
-		ifBlock.setOnMouseDragged(mouser);
-		ifBlock.setOnMouseReleased(mouser);
-		blockStorage.getChildren().add(ifBlock);
-		
+		Button run = new Button("RUN");
+		run.setMinSize(80, 50);
+		run.setAlignment(Pos.BOTTOM_LEFT);
+		layout.setRight(run);
 	}
-	
-	private class Mouser implements EventHandler<MouseEvent>{
+
+	private class Mouser implements EventHandler<MouseEvent> {
 		private BlockView current;
 		private Pane layout;
-		
-		public Mouser(Pane layout) {
+		private String id;
+
+		public Mouser(Pane layout, String id) {
 			this.layout = layout;
+			this.id = id;
 		}
-		
+
 		@Override
 		public void handle(MouseEvent event) {
 			// TODO Auto-generated method stub
-			
-			EventType<? extends MouseEvent> type = event.getEventType();
-			if(type.equals( MouseEvent.MOUSE_PRESSED)) {
-				current = new BlockView(30,40, "hi");
-				current.setLayoutX(event.getX());
-				current.setLayoutY(event.getY());
-				layout.getChildren().add(current);
-				
-			}else if(type.equals( MouseEvent.MOUSE_DRAGGED)) {
-				current.setLayoutX(event.getX());
-				current.setLayoutY(event.getY());
 
-			}else if(type.equals( MouseEvent.MOUSE_RELEASED)) {
+			EventType<? extends MouseEvent> type = event.getEventType();
+			if (type.equals(MouseEvent.MOUSE_PRESSED)) {
+
+				double mouseX = event.getSceneX();
+				double mouseY = event.getSceneY();
+				
+				current = new BlockView(100, 50, id);
+				layout.getChildren().add(current);
+				current.setLayoutX(mouseX - (current.getMinWidth() / 2));
+				current.setLayoutY(mouseY - (current.getMinHeight() / 2));
+
+			} else if (type.equals(MouseEvent.MOUSE_DRAGGED)) {
+				current.setLayoutX(event.getSceneX() - (current.getWidth() / 2));
+				current.setLayoutY(event.getSceneY() - (current.getHeight() / 2));
+
+			} else if (type.equals(MouseEvent.MOUSE_RELEASED)) {
 				BlockHandler handler = new BlockHandler(current);
 				current.setOnMousePressed(handler);
 				current.setOnMouseDragged(handler);
 				current.setOnMouseReleased(handler);
+				for(BlockView block : blocks) {
+					
+					if(Math.pow(current.getLayoutX(), 2) + Math.pow(current.getLayoutY(), 2) < 40) {
+						
+					}
+				}
 				blocks.add(current);
+				
+				
+				
+				
 				current = null;
 			}
-		}						
+		}
 	}
-	
-	private class BlockHandler implements EventHandler<MouseEvent>{
-		
+
+	private class BlockHandler implements EventHandler<MouseEvent> {
+
 		private BlockView block;
 		private double anchorX, anchorY;
-		
+
 		public BlockHandler(BlockView block) {
 			this.block = block;
 		}
-		
+
 		@Override
-		public void handle(MouseEvent event) {			
+		public void handle(MouseEvent event) {
 			EventType<? extends MouseEvent> type = event.getEventType();
-			if(type.equals( MouseEvent.MOUSE_PRESSED)) {
+			if (type.equals(MouseEvent.MOUSE_PRESSED)) {
 				anchorX = event.getX();
 				anchorY = event.getY();
-			}else if(type.equals( MouseEvent.MOUSE_DRAGGED)) {
-				
-				this.block.setLayoutX(this.block.getLayoutX()+event.getX()-anchorX);
-				this.block.setLayoutY(this.block.getLayoutY()+event.getY()-anchorY);
+			} else if (type.equals(MouseEvent.MOUSE_DRAGGED)) {
+
+				this.block.setLayoutX(this.block.getLayoutX() + event.getX() - anchorX);
+				this.block.setLayoutY(this.block.getLayoutY() + event.getY() - anchorY);
 				System.err.println("(" + event.getX() + ", " + event.getY());
-			}else if(type.equals( MouseEvent.MOUSE_RELEASED)) {
+			} else if (type.equals(MouseEvent.MOUSE_RELEASED)) {
 				anchorX = anchorY = 0;
 			}
 		}
 	}
-	
+
+	private VBox makePalette() {
+		VBox box = new VBox();
+		box.setSpacing(20);
+		box.setPadding(new Insets(30));
+		ArrayList<BlockView> paletteBlocks = new ArrayList<BlockView>();
+		paletteBlocks.add(new BlockView(100, 50, "variable"));
+		paletteBlocks.add(new BlockView(100, 50, "print"));
+		paletteBlocks.add(new BlockView(100, 50, "if"));
+		paletteBlocks.add(new BlockView(100, 50, "operator"));
+		box.setMaxSize(160, 320);
+		for (BlockView block : paletteBlocks) {
+			Mouser mouser = new Mouser(this, block.getId());
+			block.setOnMousePressed(mouser);
+			block.setOnMouseDragged(mouser);
+			block.setOnMouseReleased(mouser);
+			box.getChildren().add(block);
+		}
+
+		return box;
+	}
 }
