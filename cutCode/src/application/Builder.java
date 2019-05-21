2374d3779313be704d69466f1e2d41e25c80dffc
Arjun Khanna <arjun@Arjuns-MacBook-Air.local>
2019-05-20 17:56:11 -0700

    Fixed some variable-based math operations

--- cutCode/src/application/Builder.java
+++ cutCode/src/application/Builder.java
@@ -3,49 +3,39 @@ package application;
 import java.util.*;
 
 public class Builder {
-	ArrayList<Block<?>> allBlocks; 
+	ArrayList<Block<?>> allBlocks;
 
-	public Builder()
-	{
+	public Builder() {
 		allBlocks = new ArrayList<Block<?>>();
 	}
-	
+
 	/**
 	 * 
 	 * @param type: the type of variable. Valid types: number, boolean, string
 	 * @param value: the value of the variable
 	 * @param varName: the name of the variable
 	 */
-	public void createVariable( String varName, String value) {
-		if(value.equals("true") || value.equals("false"))
-		{
-			
+	public void createVariable(String varName, String value) {
+		if (value.equals("true") || value.equals("false")) {
+
 			VariableBlock<Boolean> v = new VariableBlock<Boolean>();
 			v.setValue(Boolean.parseBoolean(value));
 			allBlocks.add(v);
 			v.setName(varName);
-		}
-		else
-		{
-			try
-			{
+		} else {
+			try {
 				Double.parseDouble(value);
 				VariableBlock<Double> v = new VariableBlock<Double>();
 				v.setValue(Double.parseDouble(value));
 				allBlocks.add(v);
 				v.setName(varName);
-			}
-			catch(NumberFormatException e)
-			{
-				if(parseMath(value) != null)
-				{
+			} catch (NumberFormatException e) {
+				if (parseMath(value) != null) {
 					VariableBlock<Double> v = new VariableBlock<Double>();
 					v.setValue(parseMath(value));
 					allBlocks.add(v);
 					v.setName(varName);
-				}
-				else
-				{
+				} else {
 					VariableBlock<String> v = new VariableBlock<String>();
 					v.setValue(value);
 					allBlocks.add(v);
@@ -53,27 +43,25 @@ public class Builder {
 				}
 			}
 		}
-		
 
 	}
-	
-	public void print(String s)
-	{
+
+	public void print(String s) {
 		PrintBlock p = new PrintBlock();
-		if(getVariable(s) != null)
-		{
+		if (getVariable(s) != null) {
 			p.setPrint("" + getVariable(s).execute());
-		}
-		else
-		{
+		} else if (parseMath(s) != null) {
+			p.setPrint("" + parseMath(s));
+		} else {
 			p.setPrint(s);
 		}
 		allBlocks.add(p);
 	}
+
 	/**
 	 * 
 	 * @param varName
-	 * @return returns null if variable not found, 
+	 * @return returns null if variable not found,
 	 */
 	public VariableBlock<?> getVariable(String varName) {
 		for (int i = 0; i < allBlocks.size(); i++) {
@@ -89,137 +77,140 @@ public class Builder {
 	public void error() {
 		System.err.println("ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
-	
-	public void recompile()
-	{
+
+	public void recompile() {
 		allBlocks.clear();
 	}
-	
-	public String run()
-	{
+
+	public String run() {
 		String console = "";
 		String newLine = System.getProperty("line.separator");
-		
-		for(Block<?> b : allBlocks)
-		{
-			if(b instanceof PrintBlock || b instanceof IfBlock)
-			{
+
+		for (Block<?> b : allBlocks) {
+			if (b instanceof PrintBlock || b instanceof IfBlock) {
 				console = console + b.execute() + newLine;
-			}
-			else
-			{
+			} else {
 				b.execute();
 			}
 		}
-		
+
 		return console;
 	}
 
-	public void createIf(BooleanOperator<?> condition)
-	{
+	public void createIf(BooleanOperator<?> condition) {
 		IfBlock i = new IfBlock(condition.execute());
 		allBlocks.add(i);
 	}
-	
-	public void setVar(VariableBlock<String> variable, String s)
-	{
-		//TODO: fix
+
+	public void setVar(VariableBlock<String> variable, String s) {
+		// TODO: fix
 		variable.setValue(s);
 	}
-	public void setVar(VariableBlock<Double> variable, double d)
-	{
-		//TODO: fix
+
+	public void setVar(VariableBlock<Double> variable, double d) {
+		// TODO: fix
 		variable.setValue(d);
 	}
-	public void setVar(VariableBlock<Boolean> variable, boolean b)
-	{
-		//TODO: fix
+
+	public void setVar(VariableBlock<Boolean> variable, boolean b) {
+		// TODO: fix
 		variable.setValue(b);
 	}
-	public Double parseMath(String s )
-	{
-		String input = s.replaceAll("\\s+","");
-		if(input.contains("+"))
-		{
+
+	public Double parseMath(String s) {
+		String input = s.replaceAll(" ", "");
+		if (input.contains("+")) {
 			Double sum = 0.0;
 			String[] operands = input.split("\\+");
-			for(String str : operands)
-			{
-				try
-				{
+			for (String str : operands) {
+				try {
 					Double d = Double.parseDouble(str);
 					sum += d;
-				}
-				catch (NumberFormatException e)
-				{
-					return null;
+				} catch (NumberFormatException e) {
+					if (getVariable(str) != null) {
+						if (getVariable(str).execute() instanceof Double) {
+							sum += (Double) getVariable(str).execute();
+						}
+					} else {
+						return null;
+					}
 				}
 			}
 			return sum;
-		}
-		else if (input.contains("\\*"))
-		{
+		} else if (input.contains("*")) {
+			System.err.println("hello");
 			Double product = 1.0;
 			String[] operands = input.split("\\*");
-			for(String str : operands)
-			{
-				try
-				{
+			for (String str : operands) {
+				try {
 					Double d = Double.parseDouble(str);
 					product *= d;
-				}
-				catch (NumberFormatException e)
-				{
-					return null;
+				} catch (NumberFormatException e) {
+					if (getVariable(str) != null) {
+						if (getVariable(str).execute() instanceof Double) {
+							product *= (Double) getVariable(str).execute();
+						}
+					} else {
+						return null;
+					}
 				}
 			}
 			return product;
-		}
-		else if (input.contains("\\/"))
-		{
-			Double quotient = 0.0;
-			String[] operands = input.split("\\/");
-			for(String str : operands)
-			{
-				try
-				{
-					Double d = Double.parseDouble(str);
-					if(str.equals(operands[0]))
-					{
+		} else if (input.contains("/")) {
+			Double quotient = 1.0;
+			String[] operands = input.split("/");
+			for (int i = 0; i < operands.length; i++) {
+				try {
+					Double d = Double.parseDouble(operands[i]);
+					if (i == 0) {
 						quotient *= d;
-					}
-					else
-					{
+					} else {
 						quotient /= d;
 					}
-				}
-				catch (NumberFormatException e)
-				{
-					return null;
+				} catch (NumberFormatException e) {
+					if (getVariable(operands[i]) != null) {
+						if (getVariable(operands[i]).execute() instanceof Double) {
+							if (i == 0) {
+								quotient *= (Double) getVariable(operands[i]).execute();;
+							} else {
+								quotient /= (Double) getVariable(operands[i]).execute();;
+							}
+						}
+					} else {
+						return null;
+					}
 				}
 			}
 			return quotient;
-		}
-		else if (input.contains("\\-"))
-		{
+		} else if (input.contains("-")) {
 			Double difference = 0.0;
-			String[] operands = input.split("\\-");
-			for(String str : operands)
-			{
-				try
-				{
-					Double d = Double.parseDouble(str);
-					difference -= d;
-				}
-				catch (NumberFormatException e)
-				{
-					return null;
+			String[] operands = input.split("-");
+			for (int i = 0; i < operands.length; i++) {
+				try {
+					Double d = Double.parseDouble(operands[i]);
+					if (i == 0) {
+						difference += d;
+					} else {
+						difference -= d;
+					}
+				} catch (NumberFormatException e) {
+					if (getVariable(operands[i]) != null) {
+						if (getVariable(operands[i]).execute() instanceof Double) {
+							if (i == 0) {
+								difference += (Double) getVariable(operands[i]).execute();;
+							} else {
+								difference -= (Double) getVariable(operands[i]).execute();;
+							}
+						}
+					} else {
+						return null;
+					}
 				}
 			}
 			return difference;
 		}
-		
+
 		return null;
 	}
-	
+
 }
