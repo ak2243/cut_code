6aeabd4abf80ed5d41b789b104db210afe69a660
Arjun Khanna <arjunk2022@headroyce.org>
2019-12-02 19:34:51 -0800

    Tested block execute

--- cutCode/src/cutcode/test.java
+++ cutCode/src/cutcode/test.java
@@ -3,7 +3,11 @@ package cutcode;
 public class test {
 
 	public static void main(String[] args) {
-		
+		Block b = new IfBlock();
+		PrintBlock p = new PrintBlock();
+		p.setPrint("hello world");
+		((IfBlock) b).commands.add(p);
+		System.err.println(b.execute());
 	}
 
 }
