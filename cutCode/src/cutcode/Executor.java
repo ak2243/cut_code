8ab735077773e46557b7f0f48471f00bcb8c0036
Arjun Khanna <arjunk2022@headroyce.org>
2019-12-11 07:50:01 -0800

    Got some changes from master. Finished execute and run methods

--- cutCode/src/cutcode/Executor.java
+++ cutCode/src/cutcode/Executor.java
@@ -2,21 +2,28 @@ package cutcode;
 
 import java.util.List;
 
+import graphics.GraphicalBlock;
+
 public class Executor {
-	public String run(List<Sequence<Block>> sequences) {
+	/**
+	 * 
+	 * @param sequences; the List of sequences of graphical blocks
+	 * @return a String object, containing the full code
+	 * @efficiency this method is O(n^3)
+	 */
+	public String run(List<Sequence<GraphicalBlock>> sequences) {
 		String output = "";
-		for (Sequence<Block> s : sequences) {
+		for (Sequence<GraphicalBlock> s : sequences) {
 			output += execute(s);
 		}
-		return "";
+		return output;
 	}
 
-	private String execute(Sequence<Block> s) {
+	private String execute(Sequence<GraphicalBlock> s) {
 		String output = "";
-		for (Block b : s)
-		{
-			output += b.toString();
+		for (GraphicalBlock block : s) { //Need to go through all the blocks
+			output += block.getLogicalBlock().toString(); //cals the Block.toString() method to get the java code
 		}
-		return "";
+		return output;
 	}
 }
--- cutCode/src/graphics/IfBlockG.java
+++ cutCode/src/graphics/IfBlockG.java
@@ -5,22 +5,23 @@ import cutcode.*;
 public class IfBlockG extends GraphicalBlock {
 
 	private Sequence<GraphicalBlock> commands;
-	
+
 	public IfBlockG() {
 		super();
 		commands = new Sequence<GraphicalBlock>();
 	}
-	
+
 	public IfBlockG(double width, double height) {
-		super(width,height);
+		super(width, height);
 		commands = new Sequence<GraphicalBlock>();
 	}
-	
+
 	@Override
 	public Block getLogicalBlock() {
 		IfBlock ret = new IfBlock();
-		
-		return null;
+		for (GraphicalBlock g : commands) {
+			ret.commands.add(g.getLogicalBlock());
+		}
+		return ret;
 	}
-
 }
